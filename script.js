ba0db27c02e101f8893b819d6127f8b3e1bccbdf
guy <guydovlev@gmail.com>
2022-07-05 17:49:38 +0300

    Added Screen Updating

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var calculatorScreenText = document.getElementById("screen-text");
+
+
+
 // Basic Arithmetic Functions
 function add(a, b) { return a+b; }
 function subtract(a, b) { return a-b; }
@@ -10,21 +14,37 @@ function drawButtons(){
   for(let i=1; i<10; i++) {
     var numberButton = document.createElement('button');
     numberButton.innerHTML = i;
+    numberButton.addEventListener('click', function () {
+      updateScreen(i);
+    })
     buttons.appendChild(numberButton);
   }
 
   // Draw the 0 button last
   var numberButtonZero = document.createElement('button');
   numberButtonZero.innerHTML = 0;
+  numberButtonZero.addEventListener('click', function () {
+    updateScreen(0);
+  })
   buttons.appendChild(numberButtonZero);
 
   // Draw the function buttons
-  let functions = ['+', '-', 'X', '/'];
+  let functions = ['+', '-', 'x', '/', '='];
   for(let i=0; i<functions.length; i++){
     var functionButton = document.createElement('button');
     functionButton.innerHTML = functions[i];
+    functionButton.id = functions[i];
+    if(functions[i] != '='){
+      functionButton.addEventListener('click', function() {
+        updateScreen(functions[i]);
+      });
+    }
     buttons.appendChild(functionButton);
   }
+}
+
+function updateScreen(text){
+  calculatorScreenText.innerHTML += text;
 }
 
 // Return the result of the math operation
@@ -34,7 +54,7 @@ function operate(a,b,operator){
       return add(a,b);
     case '-':
       return subtract(a,b);
-    case '*':
+    case 'x':
       return multiply(a,b);
     case '/':
       return divide(a,b);
