1e5093f3e74f5da8e2783f042ef7bd0ef683eee6
guy <guydovlev@gmail.com>
2022-07-08 00:14:10 +0300

    Got the calculator to work

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
 var calculatorScreenText = document.getElementById("screen-text");
+var firstNumber = 0;
+var secondNumber = 0;
+var operator = '';
+
+var equalButton = document.getElementById("equals");
+equalButton.onclick = function () {
+  secondNumber = calculatorScreenText.innerHTML;
+  clearScreen();
+  operate(firstNumber,secondNumber,operator);
+  resetNumbers();
+}
+
+function resetNumbers() {
+  firstNumber = 0;
+  secondNumber = 0;
+  operator = '';
+}
 
 // Basic Arithmetic Functions
-function add(a, b) { return a+b; }
-function subtract(a, b) { return a-b; }
-function multiply(a, b) { return a*b; }
-function divide(a,b) { return a/b; }
+function add(a, b) { return parseFloat(a) + parseFloat(b); }
+function subtract(a, b) { return parseFloat(a) - parseFloat(b); }
+function multiply(a, b) { return parseFloat(a) * parseFloat(b); }
+function divide(a, b) { return parseFloat(a) / parseFloat(b); }
+
+
+// Return the result of the math operation
+function operate(a,b,operator){ 
+  if(operator == "plus") calculatorScreenText.innerHTML = add(a,b);
+  else if(operator == "minus") calculatorScreenText.innerHTML = subtract(a,b);
+  else if(operator == "multiply") calculatorScreenText.innerHTML = multiply(a,b);
+  else if(operator == "divide") calculatorScreenText.innerHTML = divide(a,b);
+  else calculatorScreenText.innerHTML = "error";
+}
 
 function setupButtons(){
   var buttons = document.getElementById("regular-buttons").children;
   for(let i=0; i<buttons.length; i++){
-    if(buttons[i].className == "function-button" && buttons[i].id != "equals") {
-      buttons[i].addEventListener('click', function () {
-        updateScreen(buttons[i].innerHTML);
-      });
-    } else if(buttons[i].className == "number-button"){
+    if(buttons[i].className == "number-button"){
       buttons[i].addEventListener('click' , function () {
         updateScreen(buttons[i].id);
+      });
+    }
+
+    else if(buttons[i].className == "function-button" && buttons[i].id != "equals"){
+      buttons[i].addEventListener('click', function () {
+        firstNumber = calculatorScreenText.innerHTML;
+        operator = buttons[i].id;
+        clearScreen();
       });
     }
   }
@@ -23,6 +54,8 @@ function setupButtons(){
   var clearButton = document.getElementById("clear-btn");
   clearButton.addEventListener('click', clearScreen);
 
+  var deleteButton = document.getElementById("delete-btn");
+  deleteButton.addEventListener('click', deleteChar);
 }
 
 // Screen Functions
@@ -34,22 +67,9 @@ function clearScreen(){
   calculatorScreenText.innerHTML = '';
 }
 
-
-
-// Return the result of the math operation
-function operate(a,b,operator){
-  switch(operator){
-    case '+':
-      return add(a,b);
-    case '-':
-      return subtract(a,b);
-    case 'x':
-      return multiply(a,b);
-    case '/':
-      return divide(a,b);
-    default:
-      return null;
-  }
+function deleteChar(){
+  var screenText = calculatorScreenText.innerHTML;
+  calculatorScreenText.innerHTML = screenText.slice(0, -1);
 }
 
 setupButtons();
