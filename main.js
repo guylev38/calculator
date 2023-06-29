d6659eee248c5fd7a6fa9f24fec8000ecfd70e82
Guy <guydovlev@gmail.com>
2023-06-29 19:40:29 +0300

    coded basic functionality

---
+++ main.js
@@ -0,0 +1,141 @@
+// Global Variables 
+let screenBackText = document.querySelector("#back-text");
+let screenFrontText = document.querySelector("#front-text");
+let numberButtons = document.getElementsByClassName("num-btn");
+let operatorButtons = document.getElementsByClassName("operator");
+
+let currentOperator = "";
+let firstNumber;
+let secondNumber;
+let isSolved = false;
+
+function setupButtons(){
+  // Number buttons
+  for(let i=0; i<numberButtons.length; i++){
+    numberButtons[i].addEventListener("click", () => {
+      screenFrontText.textContent += numberButtons[i].textContent;
+      if(isSolved == true) {
+        clear();
+        isSolved = false;
+        screenFrontText.textContent += numberButtons[i].textContent;
+      }
+    });
+  }
+
+  // Operators
+  for(let i=0; i<operatorButtons.length; i++){
+    operatorButtons[i].addEventListener("click", () => {
+      if(currentOperator != "") return;
+      // Display on upper screen
+      screenBackText.textContent += screenFrontText.textContent 
+        + " " 
+        + operatorButtons[i].textContent;
+
+      // Set current operator and firstNumber
+      if(isSolved == false && typeof(firstNumber) == 'number'){
+        secondNumber = Number(screenFrontText.textContent);
+        switch(currentOperator){
+          case '+':
+            screenBackText.textContent = String(add(firstNumber, secondNumber)) + " " + currentOperator;
+            firstNumber = add(firstNumber, secondNumber);
+            break;
+          case '-':
+            screenBackText.textContent = String(subtract(firstNumber, secondNumber)) + " " + currentOperator; 
+            firstNumber = subtract(firstNumber, secondNumber);
+            break;
+          case 'X':
+            screenBackText.textContent = String(multiply(firstNumber, secondNumber)) + " " + currentOperator;
+            firstNumber = multiply(firstNumber, secondNumber) + currentOperator;
+            break;
+          case '/':
+            screenBackText.textContent = String(divide(firstNumber, secondNumber)) + " " + currentOperator;
+            firstNumber = divide(firstNumber, secondNumber);
+            break;
+          case '^':
+            screenBackText.textContent = String(power(firstNumber, secondNumber)) + " " + currentOperator;
+            firstNumber = power(firstNumber, secondNumber);
+            break;
+        }
+      }
+      firstNumber = Number(screenFrontText.textContent);
+      currentOperator = operatorButtons[i].textContent;
+      screenFrontText.textContent = ""; // Clear the front text
+    });
+  } 
+
+  // floating point button
+  document.getElementById("float-dot").addEventListener("click", () => {
+    screenFrontText.textContent += ".";
+  });
+
+
+  // solve button
+  document.getElementById("solve-btn").addEventListener("click", solve);
+
+  // clear button clears everything
+  document.getElementById("clear-btn").addEventListener("click", clear);
+
+  // delete button removes last char
+  document.getElementById("delete-btn").addEventListener("click", () => {
+    screenFrontText.textContent = screenFrontText.textContent.slice(0, -1);
+  });
+}
+
+function solve() {
+  secondNumber = Number(screenFrontText.textContent);
+  screenBackText.textContent += " " + secondNumber;
+  screenFrontText.textContent = "";
+  isSolved = true;
+  switch(currentOperator){
+    case '+':
+      screenFrontText.textContent += add(firstNumber, secondNumber);
+      break;
+    case '-':
+      screenFrontText.textContent = subtract(firstNumber, secondNumber);
+      break;
+    case 'X':
+      screenFrontText.textContent = multiply(firstNumber, secondNumber);
+      break;
+    case '/':
+      screenFrontText.textContent = divide(firstNumber, secondNumber);
+      break;
+    case '^':
+      screenFrontText.textContent = power(firstNumber, secondNumber);
+      break;
+  }
+
+  
+}
+
+function clear(){
+  screenBackText.textContent = "";
+  screenFrontText.textContent = "";
+  currentOperator = "";
+  firstNumber = "";
+  secondNumber = "";
+}
+// Operator functions
+function add(a, b){
+  return a + b;
+}
+
+function subtract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b) { 
+  return a / b;
+}
+
+function power(a, b){
+  return Math.pow(a, b);
+}
+
+// Load the setup functions
+window.onload = () => {
+  setupButtons();
+}
