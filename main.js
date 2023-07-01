53cf30056fce62b1d4f6a40ff9be262df9d5f56d
Guy <guydovlev@gmail.com>
2023-07-01 14:57:23 +0300

    Revert "Finsihed refactoring"
    This reverts commit fdbc4a91b2efbeada1dee2e76b6851f944f6de54.

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ function setupButtons(){
   // Operators
   for(let i=0; i<operatorButtons.length; i++){
     operatorButtons[i].addEventListener("click", () => {
+      if(currentOperator != "") return;
       // Display on upper screen
       screenBackText.textContent += screenFrontText.textContent 
         + " " 
@@ -56,16 +57,6 @@ function setupButtons(){
             break;
         }
       }
-
-
-      if(isSolved) {
-        currentOperator = operatorButtons[i].textContent;  
-        firstNumber = Number(screenFrontText.textContent);
-        screenBackText.textContent = String(firstNumber) + " " + currentOperator;
-        isSolved = false;
-      }
-
-
       firstNumber = Number(screenFrontText.textContent);
       currentOperator = operatorButtons[i].textContent;
       screenFrontText.textContent = ""; // Clear the front text
@@ -76,6 +67,7 @@ function setupButtons(){
   document.getElementById("float-dot").addEventListener("click", () => {
     screenFrontText.textContent += ".";
   });
+
 
   // solve button
   document.getElementById("solve-btn").addEventListener("click", solve);
@@ -111,6 +103,8 @@ function solve() {
       screenFrontText.textContent = power(firstNumber, secondNumber);
       break;
   }
+
+  
 }
 
 function clear(){
